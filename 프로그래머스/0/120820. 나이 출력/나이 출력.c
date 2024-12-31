4218b876f21be988759e50b524491416b529af94
Aiden Shin <shinichi9784@gmail.com>
2024-12-31 10:31:54 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    int year = 2022;
+    int birth = year+1-age;
+    answer = birth;
+    return answer;
+}
